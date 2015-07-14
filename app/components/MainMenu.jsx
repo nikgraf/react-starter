272d5ba374a464ebfd95e093597f81cd29306697
Nik Graf <nikgraf.com@gmail.com>
2015-07-14 03:14:15 +0200

    feat(Button, Toggle): add component to the main page

--- app/components/MainMenu.jsx
+++ app/components/MainMenu.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import { Button, Toggle } from "belle";
 import ReactLogo from "elements/ReactLogo";
 
 export default class MainMenu extends React.Component {
@@ -7,12 +8,15 @@ export default class MainMenu extends React.Component {
 		return <div>
 			<ReactLogo type="svg" /> <ReactLogo type="png" /> <ReactLogo type="jpg" />
 			<h2>MainMenu:</h2>
+			<Button primary>Lala</Button>
+			<Toggle />
 			<ul>
 				<li>The <Link to="home">home</Link> page.</li>
 				<li>Do something on the <Link to="todo">todo page</Link>.</li>
 				<li>Switch to <Link to="some-page">some page</Link>.</li>
 				<li>Open the chat demo: <Link to="chat" params={{room: "home"}}>Chat</Link>.</li>
 				<li>Open the page that shows <Link to="readme">README.md</Link>.</li>
+
 			</ul>
 		</div>;
 	}
